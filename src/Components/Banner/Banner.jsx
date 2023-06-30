5ec8179d14271e8d71899bb266105f9f964f6459
Lucas Maclean <contatolucasmaclean@gmail.com>
2023-06-30 17:27:49 -0300

    feat: added structure and style of CardTempoReal component

--- src/Components/Banner/Banner.jsx
+++ src/Components/Banner/Banner.jsx
@@ -1,3 +1,5 @@
+import CardTempoReal from "../CardTempoReal/CardTempoReal";
+
 export default function Banner() {
   return (
     <section className="bg-zinc-400 mb-6 h-80 flex relative rounded-2xl p-8 bg-[url('src/assets/img/bannerSoccer.svg')] bg-no-repeat bg-cover bg-center">
@@ -12,17 +14,7 @@ export default function Banner() {
         </span>
       </div>
       <div className="w-2/4 h-full flex items-center justify-end gap-4">
-        <div className="w-2/3 bg-chumbo shadow-boxShadow p-6 rounded-2xl flex flex-col items-center justify-center gap-4">
-          <h2 className="text-white text-center text-2xl font-medium">
-            Passando Agora
-          </h2>
-          <div className="flex items-center justify-center gap-6">
-            <img src="" alt="" />
-            <span className="text-white text-[2rem] font-bold">3 x 0</span>
-            <img src="" alt="" />
-          </div>
-          <p className="text-white font-medium text-base">60:00</p>
-        </div>
+        <CardTempoReal />
       </div>
     </section>
   );
